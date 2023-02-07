5533a38adfb9a64b48ff88e0b75ebfa0ad6180e2
Player10202 <114907799+Player10202@users.noreply.github.com>
2023-02-07 19:15:41 +1100

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,23 @@
+class Ground{
+    constructor (x,y,w,h){
+        let options={
+            isStatic:true
+        }
+
+        this.body=Bodies.rectangle(x,y,w,h,options)
+        this.width=w
+        this.height=h
+        World.add(world,this.body)
+    }
+
+    show(){
+    let pos=this.body.position
+    push()
+    rectMode(CENTER)
+    fill(148,127,146)
+    rect(pos.x,pos.y,this.width,this.height)
+    pop()
+    
+    }
+
+}
